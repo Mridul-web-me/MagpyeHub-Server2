5a7914819dfd91b01f906e27617b529cce701377
Mridul-web-me <md.mridu.23@gmail.com>
2022-08-03 21:43:41 +0600

    currency updated

--- index.js
+++ index.js
@@ -418,7 +418,7 @@ async function run() {
             const paymentInfo = req.body;
             const amount = paymentInfo.total * 100;
             const paymentIntent = await stripe.paymentIntents.create({
-                currency: 'gbp',
+                currency: 'USD',
                 amount: amount,
                 payment_method_types: ['card'],
             });
